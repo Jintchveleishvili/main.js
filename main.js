e657c9af404f0ea74dff4208102f4a365854124d
Magda Chikhradze <magda292005@gmail.com>
2026-03-24 18:55:58 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// let number=20
+// let age=30
+// let chosenAge='30'
+// let text1='this is some text'
+// let text2='this is some text'
+// console.log(text1 == text2)
+// console.log(text1 === text2)
+// console.log(age == chosenAge)
+// console.log(age === chosenAge)
+
+// console.log(20 >=10)
+// console.log(30%11)
+// console.log(20%8===0)
+// let compare = 20 == '20'
+// let compare1 = 20 === '20'
+// console.log(compare)
+// console.log(compare1)
+// let compare2=20+'1'
+// console.log(compare2)
+// let compare3=20-'1'
+// console.log(compare3)
+// let compare4= 20 / '4'
+// console.log(compare4)
+// let compare5=20-"6"
+// console.log(compare5)
+
+// if(20>10){console.log('this condition is true')}
+// console.log(2===2 && 3>0)
+// let myAge=12
+// if(myAge>=18 && myAge<120){console.log('მომხმარებელი არის სრულწლოვანი')}
+// // else{console.log('მომხმარებელი არის არასრულწლოვანი')}
+// // console.log('continue')
+// else if(myAge>0 && myAge<18){console.log("მომხმარებელი არის არასრულწლოვანი")
+
+// }else{console.log('შეყვანილი ასაკი არასწორია')}
+// let score=-3
+// if (score>90 && score<=100){console.log('A')}
+// else if (score>80 && score<=90){console.log('B')}
+// else if (score>70 && score<=80){console.log('C')}
+// else if (score>60 && score<=70){console.log('D')}
+// else if (score>50 && score<=60){console.log('F')}
+// else if (score>=0 && score<=50){console.log('FX')}
+// else{console.log("შეყვანილი მონაცემი არასწორია")}
+
+// let num=0
+// if (num%2===0){console.log('ლუწი')}
+  
+//         else {console.log('კენტი')}
+
+// let int=0
+// if(int>0){console.log('dadebithi')}
+// else if (int<0){console.log('uaryofithi')}
+// else {console.log(0)}
+
+let health= -1
+if (0<=health && health<=10){console.log('gaagrdzele tamashi')}
+else if (health<0){console.log('araswori monacemia')}
+else{console.log('game over')}
