7e174d1e1ae4da9dacea92555f6b4f1222ead721
boris <borisjinchvel@gmail.com>
2026-03-25 15:46:00 +0400

    first commit

---
+++ main1.js
@@ -0,0 +1,60 @@
+// let isRain = false
+// if(isRain===true){
+// console.log('darchi saxlshi')}
+// else{console.log('gadi garet')}
+
+// let age=65
+// if (age>=65){console.log('gekutvnit pensia')}
+// else {console.log('ar gekutvniT')}
+
+// let myAge=25
+// if(myAge>18 && myAge<30){console.log('Valid age')}
+
+// console.log(1==true)
+// console.log(0==false)
+// console.log(null==false)
+// console.log(undefined==false)
+// console.log(''==false)
+// console.log(Boolean('underfind'))
+// console.log(23+'222')
+// console.log(23+'dfgjy')
+// console.log(23-'222')
+// console.log(23-'uhj')
+// console.log(Number('205'))
+// let student=true
+// let studentAge=22 
+// if(student && studentAge>20){console.log('gekuTvniT fasdakleba')}
+// let student=false
+// let studentAge=22 
+// if(student && studentAge>20){console.log('gekuTvniT fasdakleba')}
+// else{console.log('ar gekuTvniT fasdakleba')}
+// let student=true
+// let studentAge=18
+// if(student && studentAge>20){console.log('gekuTvniT fasdakleba')}
+// else{console.log('ar gekuTvniT fasdakleba')}
+// let student=false
+// let studentAge=18
+// if(student && studentAge>20){console.log('gekuTvniT fasdakleba')}
+// else{console.log('ar gekuTvniT fasdakleba')}
+// let student=true
+// let studentAge=18
+// if(student || studentAge>20){console.log('gekuTvniT fasdakleba')}
+// else{console.log('ar gekuTvniT fasdakleba')}
+// let student=false
+// let studentAge=18
+// if(student || studentAge>20){console.log('gekuTvniT fasdakleba')}
+// else{console.log('ar gekuTvniT fasdakleba')}
+
+// let pay=false
+// let VIP=false
+// if(pay||VIP){console.log('shegidzliat shexvidet kinoshi')}
+// else{console.log('ar shegidzliat shexvidet kinoshi')}
+// let isLoggedIn=false
+// if(!isLoggedIn){console.log('login')}
+// else{console.log('welcome')}
+
+let isLoggedIn=true
+if(!isLoggedIn){console.log('login')}
+else{console.log('welcome')}
+
+
